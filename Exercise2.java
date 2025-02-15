e61df0dde0b00ceb98ced917da2253269f469ccf
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2025-02-15 17:58:25 -0500

    Create Exercise2.java

---
+++ Exercise2.java
@@ -0,0 +1,12 @@
+// Do not modify this code
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        Rectangle rect1 = new Rectangle(5, 10);
+        Rectangle rect2 = new Rectangle(7); // Square
+        System.out.println(rect1);
+        System.out.println(rect2);
+        System.out.println("Area of rect1: " + rect1.getArea());
+        System.out.println("Area of rect2: " + rect2.getArea());
+    }
+}
