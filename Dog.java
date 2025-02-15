8633b859ae0e4d5cb00e345e18a55d1bb0a37756
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2025-02-15 18:01:08 -0500

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -3,4 +3,13 @@
 class Dog {
   // Write your code here
 
+
+  public void bark() {
+      System.out.println("Woof!");
+  }
+
+  @Override
+  public String toString() {
+      return "Name: " + name + ", Breed: " + breed + ", Age: " + age;
+  }
 }
