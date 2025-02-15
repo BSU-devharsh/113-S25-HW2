822c1f9e9434a24e1cdb7e34009fdfff02f1ebd0
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2025-02-15 17:57:44 -0500

    Create Exercise3.java

---
+++ Exercise3.java
@@ -0,0 +1,10 @@
+// Do not modify this code
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        Person person1 = new Person(); // Uses the default constructor
+        Person person2 = new Person("Alice", 25);
+        System.out.println(person1);
+        System.out.println(person2);
+    }
+}
