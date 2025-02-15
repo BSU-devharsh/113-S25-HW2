2541b42adb48adcfe9eda5a726c4e068bcac31b7
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2025-02-15 17:59:40 -0500

    Create Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,9 @@
+// Do not modify this code
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Dog myDog = new Dog("Buddy", "Golden Retriever", 3);
+        System.out.println(myDog);
+        myDog.bark();
+    }
+}
